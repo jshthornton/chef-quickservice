99474cdc025b3714f33c2044f5e17a6f06fa3bf9
Joshua Thornton <joshua@techinasia.com>
2015-10-28 15:08:20 +0800

    Support multiple services and actions.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-service node[:quickservice][:service] do
-  action node[:quickservice][:action]
+services = [node[:quickservice][:service]] unless node[:quickservice][:service].kind_of(Array)
+actions = [node[:quickservice][:action]] unless node[:quickservice][:action].kind_of(Array)
+
+actions.fill(actions.last, actions.length..services.length-1)
+
+services.each_index do |i|
+  service services[i] do
+    action actions[i]
+  end
 end
